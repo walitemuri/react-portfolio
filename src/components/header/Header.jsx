6c27efbe8307695fc3a2b3ab11b620224c50c7d9
walitemuri <wmtemuri@gmail.com>
2022-12-02 21:34:08 -0500

    Header Component complete

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Wali Temuri</h1>
+        <h5 className="text-light">Junior Developer</h5>
+        <CTA />
+        <HeaderSocials />
+
+        <div>
+          <img src={ME} alt="me" className='me'/>
+        </div>
+          <a href="#contact" className='scroll_down'>Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './nav.css'
+import {MdHomeFilled} from 'react-icons/md'
+import {FaUserTie} from 'react-icons/fa'
+import {AiOutlineFundProjectionScreen} from 'react-icons/ai'
+import {FaHandshake} from 'react-icons/fa'
+import {MdEmail} from 'react-icons/md'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#"><MdHomeFilled/></a>
+      <a href="#about"><FaUserTie/></a>
+      <a href="#portfolio"><AiOutlineFundProjectionScreen/></a>
+      <a href="#services"><FaHandshake/></a>
+      <a href="#contact"><MdEmail/></a>
+    </nav>
   )
 }
 
