40ac225dc5ed1df42198e470c42a843719d8f2da
walitemuri <wmtemuri@gmail.com>
2022-12-14 09:16:39 -0500

    Styling contact.css Complete

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import './contact.css'
-
+import {MdOutlineEmail} from 'react-icons/md'
+import {FaInstagram} from 'react-icons/fa'
+import {CiLinkedin} from 'react-icons/ci'
+ 
 const Contact = () => {
   return (
     <section id='contact'>
@@ -10,21 +13,31 @@ const Contact = () => {
       <div className="container contact__container">
         <div className="contact__options">
           <article className="contact__option">
+            <MdOutlineEmail className='contact__option-icon'/>
             <h4>Email</h4>
-            <h5>dummyemail@gmail.com</h5>
-            <a href="mailto:dummyemail@gmail.com">Send a message</a>
+            <h5>walitemuri@gmail.com</h5>
+            <a href="mailto:walitemuri@gmail.com" target="_blank">Send a message</a>
           </article>
           <article className="contact__option">
+            <FaInstagram className='contact__option-icon'/>
             <h4>Instagram</h4>
-            <h5>dummyemail@gmail.com</h5>
-            <a href="mailto:dummyemail@gmail.com">Send a message</a>
+            <h5>@walitemuri</h5>
+            <a href="https://api.instagram.com/walitemuri" target="_blank">Send a message</a>
           </article>
           <article className="contact__option">
+            <CiLinkedin className='contact__option-icon'/>
             <h4>LinkedIn</h4>
             <h5>dummyemail@gmail.com</h5>
-            <a href="mailto:dummyemail@gmail.com">Send a message</a>
+            <a href="https://linkedin.com/walitemuri" target="_blank">Send a message</a>
           </article>
         </div>
+        {/* END OF CONTACTS SECTION */}
+        <form action="">
+          <input type="text" name='name' placeholder='Your Full Name' required/>
+          <input type="email" name='email' placeholder='Your Email' required/>
+          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
       </div>
     </section>
   )
