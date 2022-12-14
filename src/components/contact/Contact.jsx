3dde8c9321b90fb90295302472e87bf5255ddbca
walitemuri <wmtemuri@gmail.com>
2022-12-14 03:57:37 -0500

    Test commit

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,7 +3,30 @@ import './contact.css'
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <h4>Email</h4>
+            <h5>dummyemail@gmail.com</h5>
+            <a href="mailto:dummyemail@gmail.com">Send a message</a>
+          </article>
+          <article className="contact__option">
+            <h4>Instagram</h4>
+            <h5>dummyemail@gmail.com</h5>
+            <a href="mailto:dummyemail@gmail.com">Send a message</a>
+          </article>
+          <article className="contact__option">
+            <h4>LinkedIn</h4>
+            <h5>dummyemail@gmail.com</h5>
+            <a href="mailto:dummyemail@gmail.com">Send a message</a>
+          </article>
+        </div>
+      </div>
+    </section>
   )
 }
 
