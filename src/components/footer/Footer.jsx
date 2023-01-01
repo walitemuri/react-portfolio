b4407a6f8c57f1ab6133d0a79efda485e3406458
walitemuri <wmtemuri@gmail.com>
2023-01-01 18:35:34 -0500

    Revert "Revert "Finished Footer Section""
    This reverts commit f5596f44b3c1d43338c5e22022e685087d8b932a.

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 import './footer.css'
+import {FaTwitter} from 'react-icons/fa'
+import {BsLinkedin} from 'react-icons/bs'
+import {FaInstagram} from 'react-icons/fa'
 
 const Footer = () => {
   return (
-    <section>Footer</section>
+    <footer>
+      <a href="#" className='footer__logo'>WaliT</a>
+
+      <ul className="permaLinks">
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+      
+      <div className="footer__socials">
+        <a href="https://instagram.com"><FaInstagram/></a>
+        <a href="https://twitter.com"><FaTwitter/></a>
+        <a href="https://linkedin.com"><BsLinkedin/></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Temuri Company Ltd. All rights reserved.</small>
+      </div>
+    </footer>
   )
 }
 
